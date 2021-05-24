51c72341d0cc4246e09d81bebe70de4bb4194871
Atur Gupta <aturgupta08@gmail.com>
2021-05-24 20:05:00 +0530

    Update hash.py

--- Assignment1/hash.py
+++ Assignment1/hash.py
@@ -1,27 +1,24 @@
 import hashlib
 import time
 
-ogstr = input("Enter String: ")
-encoded = ogstr.encode() # convert to binary 
-oghash = hashlib.sha256(encoded) # SHA 256 object
-oghashstr = str(oghash.hexdigest()) # hash string 
+ogstr = 'Atur'
 x = 0; 
 start = time.time()
 while(1):
     x += 1
     strx = str(x)
     curstr = ogstr + strx 
-    encoded = curstr.encode()
-    curhash = hashlib.sha256(encoded)
-    curhashstr = str(curhash.hexdigest())
+    curstr_encoded = curstr.encode()
+    hash = hashlib.sha256(curstr_encoded)
+    hashstr = str(hash.hexdigest())
     for i in range(5):
-        if(curhashstr[i] != '0'):
+        if(hashstr[i] != '0'):
             break; 
     else:
         t = "00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-        if(curhashstr != t):
+        if(hashstr != t):
             print(f'Required number: {x}')
-            print(f'SHA 256 hash of the string "{curstr}" is {curhashstr}')
+            print(f'SHA 256 hash of the string "{curstr}" is {hashstr}')
             print(f'Time taken: {time.time() - start} seconds') 
             break
 
